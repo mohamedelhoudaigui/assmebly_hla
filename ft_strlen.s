165ab1d09cf35ea5843d45e7b12d1ecf27aab29d
Mohamed El Houdaigui <mel-houd@e3r1p7.1337.ma>
2024-08-07 09:11:37 +0100

    lazy

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,12 +1,13 @@
 section .data
-msg db 'Hello from function!', 0
+	msg db 'test register', 0
 
 section .text
 global _start
 
 _start:
     ; Call the function
-    call my_function
+	mov rdi, msg
+    call print
 
     ; Exit the program
     mov eax, 1          ; syscall number for sys_exit
@@ -14,11 +15,11 @@ _start:
     int 0x80            ; interrupt to invoke syscall
 
 ; Define the function
-my_function:
+print:
     ; Print the message
     mov eax, 4          ; syscall number for sys_write
     mov ebx, 1          ; file descriptor 1 (stdout)
-    mov ecx, msg        ; pointer to the message
+    mov ecx, [rdi]        ; pointer to the message
     mov edx, 21         ; message length
     int 0x80            ; interrupt to invoke syscall
 
